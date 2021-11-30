cfa1beeab45aa57004b62acbb6f9ffa1a4cd2b0f
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-11-30 13:33:40 -0500

    Feature: Primer componente

---
+++ src/MiPrimerComponente.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+export default function MiPrimerComponente(){
+    return(
+        <> 
+            <h3>Mi primer Componente Funcional</h3>
+            <span>Cesar Andres Abadia Dávila</span>
+        </>
+
+    );
+}
+
+
+
